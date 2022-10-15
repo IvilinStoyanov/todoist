c30c187931fd21e3c9625c8670e6cb4c1ddcd584
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2022-10-15 16:35:14 +0300

    adding ng-bootstrap

---
+++ src/app/components/common/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  links = [
+    { title: 'One', fragment: 'one' },
+    { title: 'Two', fragment: 'two' }
+  ];
+
+
+  constructor(public route: ActivatedRoute) { }
+
+  ngOnInit() {
+  }
+
+}
